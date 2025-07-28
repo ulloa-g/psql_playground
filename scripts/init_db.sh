6ce326b766066d6febbdee786d91765c598a7b74
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-07-28 03:35:16 -0300

    pupulate characters table

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,11 +2,11 @@
 
 
 PGUSER=tu_nombre_de_usuario # Reemplaza con tus credenciales
-
-PGPASSWORD=tu_contraseña # # Reemplaza con tus credenciales
+PGPASSWORD=tu_contraseña # Reemplaza con tus credenciales
 DBNAME=mario_db_test
 
 PGPASSWORD=$PGPASSWORD psql -U $PGUSER -h localhost -d postgres -c "CREATE DATABASE $DBNAME;"
 
 PGPASSWORD=$PGPASSWORD psql -U $PGUSER -h localhost -d "$DBNAME" -f ../sql/01_create_schema.sql
 PGPASSWORD=$PGPASSWORD psql -U $PGUSER -h localhost -d "$DBNAME" -f ../sql/02_create_tables.sql
+PGPASSWORD=$PGPASSWORD psql -U $PGUSER -h localhost -d "$DBNAME" -f ../sql/03_populate_tables.sql
---
+++ sql/03_populate_tables.sql
@@ -0,0 +1,12 @@
+SET search_path TO mariobross;
+
+INSERT INTO characters (name, homeland, favorite_color) VALUES
+('Mario', 'Mushroom Kingdom', 'Red'),
+('Luigi', 'Mushroom Kingdom', 'Green'),
+('Peach', 'Mushroom Kingdom', 'Pink'),
+('Bowser', 'Koopa Kingdom', 'Yellow'),
+('Toad', 'Mushroom Kingdom', 'Blue'),
+('Yoshi', 'Dinosaur Land', 'Green'),
+('Wario', 'Diamond City', 'Yellow'),
+('Daisy', 'Sarasaland', 'Orange'),
+('Waluigi', 'Diamond City', 'Purple');

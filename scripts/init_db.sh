f5cff28a546952ba3cb620188a96579f796ce4f9
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-07-28 06:52:13 -0300

    fixed hardcode of credentials

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# cargar credenciales y variables de entorno desde .env
+set -o allexport
+source ../.env
+set +o allexport
 
-PGUSER=tu_nombre_de_usuario # Reemplaza con tus credenciales
-PGPASSWORD=tu_contraseña # Reemplaza con tus credenciales
-DBNAME=mario_db
-
+# crear base de datos si no existe
 PGPASSWORD=$PGPASSWORD psql -U $PGUSER -h localhost -d postgres -c "CREATE DATABASE $DBNAME;"
 
+# ejecutar scripts SQL para crear esquema y tablas
 PGPASSWORD=$PGPASSWORD psql -U $PGUSER -h localhost -d "$DBNAME" -f ../sql/01_create_schema.sql
 PGPASSWORD=$PGPASSWORD psql -U $PGUSER -h localhost -d "$DBNAME" -f ../sql/02_create_tables.sql
 PGPASSWORD=$PGPASSWORD psql -U $PGUSER -h localhost -d "$DBNAME" -f ../sql/03_populate_tables.sql
--- scripts/run_queries.sh
+++ scripts/run_queries.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# cargar credenciales y variables de entorno desde .env
+set -o allexport
+source ../.env
+set +o allexport
 
-PGUSER=tu_nombre_de_usuario # Reemplaza con tus credenciales
-PGPASSWORD=tu_contraseña # Reemplaza con tus credenciales
-DBNAME=mario_db
-
+# ejecutar consultas de ejemplo
 PGPASSWORD=$PGPASSWORD psql -U $PGUSER -h localhost -d "$DBNAME" -f ../sql/04_example_queries.sql

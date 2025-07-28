aabd754947f0b296a2eaed1a6ffa7842c2556d04
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-07-28 04:11:05 -0300

    work on more_info table

--- sql/02_create_tables.sql
+++ sql/02_create_tables.sql
@@ -2,7 +2,15 @@ SET search_path TO mariobross;
 
 CREATE TABLE IF NOT EXISTS characters (
     character_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(50) UNIQUE NOT NULL,
     homeland VARCHAR(50) NOT NULL,
     favorite_color VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS more_info(
+    more_info_id SERIAL PRIMARY KEY,
+    birthday DATE,
+    height_cm INT,
+    weight_kg NUMERIC(4, 1),
+    character_id INT REFERENCES characters(character_id) ON DELETE CASCADE UNIQUE NOT NULL
 );
--- sql/03_populate_tables.sql
+++ sql/03_populate_tables.sql
@@ -9,4 +9,17 @@ INSERT INTO characters (name, homeland, favorite_color) VALUES
 ('Yoshi', 'Dinosaur Land', 'Green'),
 ('Wario', 'Diamond City', 'Yellow'),
 ('Daisy', 'Sarasaland', 'Orange'),
-('Waluigi', 'Diamond City', 'Purple');
+('Waluigi', 'Diamond City', 'Purple')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO more_info (birthday, height_cm, weight_kg, character_id) VALUES
+('1981-07-09', 155, 64.5, 1), -- Mario
+('1983-12-01', 152, 68.0, 2), -- Luigi
+('1985-07-21', 165, 55.0, 3), -- Peach
+('1985-07-21', 258, 300, 4), -- Bowser
+('1988-11-21', 100, 30.0, 5), -- Toad
+('1990-07-21', 120, 45.0, 6), -- Yoshi
+('1992-06-23', 170, 80.0, 7), -- Wario
+('1992-06-23', NULL, NULL, 8), -- Daisy
+('2000-04-01', 175, 75.0, 9) -- Waluigi
+ON CONFLICT (character_id) DO NOTHING;
